a2049c270c7f734634dbd8cf767667f06b758ffc
millen1m <maxim.millen@gmail.com>
2016-05-12 18:11:48 +1200

    ROI calculator is working - display is poor.

---
+++ calculate-roi.js
@@ -0,0 +1,31 @@
+/**
+ * Created by maximmillen on 12/05/16.
+ */
+
+function calculateROI() {
+        console.log("triggered calculate roi");
+  var theForm = document.forms["roi-calculator"];
+  var number_engs = theForm.elements["num-engs"].value;
+  var hours_week = theForm.elements["hours"].value;
+  var eng_cost = 80;
+  var work_weeks = 47;
+  var annual_cost = eng_cost * hours_week * work_weeks * number_engs;
+  var review_ratio = 0.5;
+  var reduction_ratio = 0.3;
+  var pensolve_savings = annual_cost * review_ratio * reduction_ratio;
+  var savings_str = "$" + pensolve_savings.toFixed(2).replace(/./g, function(c, i, a) {
+    return i && c !== "." && ((a.length - i) % 3 === 0) ? ',' + c : c;
+});
+  var cost_str = "$" + pensolve_savings.toFixed(2).replace(/./g, function(c, i, a) {
+    return i && c !== "." && ((a.length - i) % 3 === 0) ? ',' + c : c;
+});
+
+  var divobj = document.getElementById('roi-answers');
+  divobj.style.display = "block";
+  divobj.innerHTML = "The annual cost of spreadsheet engineering: " + cost_str;
+
+  console.log("annual cost: ", annual_cost);
+  console.log("pensolve savings: ", pensolve_savings);
+  console.log("savings str: ", savings_str);
+
+      }
