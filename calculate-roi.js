9de809474fefac9c1ba587efbffb3f5cf14db75e
millen1m <maxim.millen@gmail.com>
2016-05-12 18:57:57 +1200

    ROI calculator is working.

--- calculate-roi.js
+++ calculate-roi.js
@@ -7,22 +7,25 @@ function calculateROI() {
   var theForm = document.forms["roi-calculator"];
   var number_engs = theForm.elements["num-engs"].value;
   var hours_week = theForm.elements["hours"].value;
-  var eng_cost = 80;
+  var eng_cost = parseFloat(theForm.elements["rate"].value.replace("$", ""));
+  console.log("cost: ", eng_cost);
   var work_weeks = 47;
   var annual_cost = eng_cost * hours_week * work_weeks * number_engs;
   var review_ratio = 0.5;
   var reduction_ratio = 0.3;
   var pensolve_savings = annual_cost * review_ratio * reduction_ratio;
-  var savings_str = "$" + pensolve_savings.toFixed(2).replace(/./g, function(c, i, a) {
+  var savings_str = "$" + pensolve_savings.toFixed(0).replace(/./g, function(c, i, a) {
     return i && c !== "." && ((a.length - i) % 3 === 0) ? ',' + c : c;
 });
-  var cost_str = "$" + pensolve_savings.toFixed(2).replace(/./g, function(c, i, a) {
+  var cost_str = "$" + annual_cost.toFixed(0).replace(/./g, function(c, i, a) {
     return i && c !== "." && ((a.length - i) % 3 === 0) ? ',' + c : c;
 });
 
   var divobj = document.getElementById('roi-answers');
   divobj.style.display = "block";
-  divobj.innerHTML = "The annual cost of spreadsheet engineering: " + cost_str;
+  document.getElementById('annual-cost').innerHTML = "The annual cost of spreadsheet engineering: " + cost_str;
+
+  document.getElementById('annual-savings').innerHTML = "Pensolve annual savings: " + savings_str;
 
   console.log("annual cost: ", annual_cost);
   console.log("pensolve savings: ", pensolve_savings);
